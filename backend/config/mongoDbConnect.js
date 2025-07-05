363613503eb569d430ed354229bd38594ce14d2a
sandeshKatkar <sandeshkatkar143@gmail.com>
2025-07-05 17:54:18 +0530

    Bakend : User routes & api & Product routes & api are created

---
+++ backend/config/mongoDbConnect.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const connectDB=async()=>{
+
+
+console.log("Connecting to MongoDB with URI:", process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log("✅ MongoDB connected successfully");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.error("❌ MongoDB connection error:", err);
+  });
+
+
+   try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce-mern`);
+   } catch (error) {
+    console.log(error)
+   }
+}
+
+export default connectDB;
---
+++ backend/server.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import dotenv from 'dotenv';
+import connectDB from './config/mongoDbConnect.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/user.routes.js'
+import productRouter from './routes/product.routes.js'
+
+// app configration
+const app=express()
+const port=4000
+
+app.use(express.urlencoded({ extended: true }));
+
+
+
+dotenv.config();
+
+// connect to DB
+connectDB();
+
+//connect cloudinary
+connectCloudinary();
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+
+// api end points
+
+app.use('/api/user',userRouter);
+
+app.use('/api/product',productRouter)
+
+app.get("/",(req,res)=>{
+    
+    res.send("Server is Running !")
+
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Your app is running on ${port}`)
+})
